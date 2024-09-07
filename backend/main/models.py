6189f8b250124e2a6c84e3875619fbd2126b81f2
fridalif <fridalif>
2024-09-07 16:24:13 +0300

    created coutry model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название', verbose_name_plural='Названия')
+    flag = models.ImageField(verbose_name='Флаг', null=True, blank=True, upload_to='flags', verbose_name_plural='Флаги')
+    world_part = models.CharField(max_length=100, verbose_name='Часть света', 
+                                  choices=[('eu', 'Европа'), ('as', 'Азия'), ('af', 'Африка'), ('am', 'Америка'), ('oc', 'Океания')])
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Страна'
+        verbose_name_plural = 'Страны'
+        table_name = 'country'
+
+
