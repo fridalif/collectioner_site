57cc2021dbb3249c91ecb079761f8761672e9aef
fridalif <fridalif>
2024-09-07 21:33:37 +0300

    created validators is int and is float

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from django.http import HttpRequest
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from main.models import Glue, Color, Stamp, Format, Country, Theme, Press, Emission, Designer, Catalog, Currency, Watermark, Item
+
+def is_int(obj):
+    if obj is None:
+        return False
+    try:
+        obj = int(obj)
+        return True
+    except:
+        return False
+
+def is_float(obj):
+    if obj is None:
+        return False
+    try:
+        obj = float(obj)
+        return True
+    except:
+        return False
+
+@api_view(['GET'])
+def get_items(request:HttpRequest)->Response:
+    limit = request.GET.get('limit')
+    offset = request.GET.get('offset')
+    if limit is None:
+        limit = 10
+    if limit > 100:
+        limit = 100
+    if offset is None:
+        offset = 0
