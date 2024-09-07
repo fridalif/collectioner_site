a392e2a7cd911bf580961d7bc2650e72f7289f81
fridalif <fridalif>
2024-09-07 21:38:23 +0300

    comment

--- backend/api/views.py
+++ backend/api/views.py
@@ -25,11 +25,16 @@ def is_float(obj):
 
 @api_view(['GET'])
 def get_items(request:HttpRequest)->Response:
+    # Пагинация + валидация limit и offset
     limit = request.GET.get('limit')
     offset = request.GET.get('offset')
-    if limit is None:
+    if not is_int(limit):
         limit = 10
+    else:
+        limit = int(limit)
     if limit > 100:
         limit = 100
-    if offset is None:
+    if not is_int(offset) or int(offset)<0:
         offset = 0
+    else:
+        offset = int(offset)
