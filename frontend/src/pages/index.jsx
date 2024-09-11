c114b5366e5ccb818354be1ceb88c66796fbe5e9
fridalif <fridalif>
2024-09-11 22:31:03 +0300

    initialized header files

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Home(){
 
     return (
-        <div className="main" style={{'display':'flex','height':'100%','width':"100%",'alignItems':'center','flexDirection':'column','gap':'30px'}}>
+        <div className="main" style={{'display':'flex','height':'100%','width':"100%",'alignItems':'center','flexDirection':'column','gap':'30px','position':'absolute'}}>
             Hello wordl!
         </div>
     )
