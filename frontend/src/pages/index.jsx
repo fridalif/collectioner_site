dff10a1093c9133d94f0d244253ecec9165a0bb5
fridalif <fridalif>
2024-09-11 22:18:25 +0300

    intitalized index.jsx

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+
+    return (
+        <div className="main" style={{'display':'flex','height':'100%','width':"100%",'alignItems':'center','flexDirection':'column','gap':'30px'}}>
+            Hello wordl!
+        </div>
+    )
+}
