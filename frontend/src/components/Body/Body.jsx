d3bc312e595a190ec712af996abb8a63e2400151
fridalif <fridalif>
2024-09-11 23:37:35 +0300

    Created welcome message block

--- frontend/src/components/Body/Body.jsx
+++ frontend/src/components/Body/Body.jsx
@@ -1,10 +1,13 @@
 import styles from './Body.module.css';
 import {SecondHeader} from '../SecondHeader/SecondHeader.jsx';
+import {WelcomeMessage} from '../WelcomeMessage/WelcomeMessage.jsx';
+
 export function Body(){
 
     return(
         <div className={styles.body}>
             <SecondHeader />
+            <WelcomeMessage />
         </div>
     )
 }
