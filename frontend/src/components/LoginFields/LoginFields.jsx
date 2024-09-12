d24434c9a5d0f745b0aab3bb27584ea52319aa30
fridalif <fridalif>
2024-09-13 00:43:35 +0300

    ended designing of login and register forms

---
+++ frontend/src/components/LoginFields/LoginFields.jsx
@@ -0,0 +1,57 @@
+import styles from './LoginFields.module.css'
+
+export function LoginFields(){
+    return(
+        <>
+            <div className={styles.loginHeader}>
+                Вход
+            </div>
+            <input className={styles.loginInput} type='text' placeholder='Логин или почта'/>
+            <input className={styles.loginInput} type='password' placeholder='Пароль'/>
+        </>
+    )
+}
+
+export function RegisterFields(){
+    return(
+        <>
+            <div className={styles.loginHeader}>
+                Регистрация
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.label} for='login'> Логин </label> 
+                <input className={styles.registerInput} type='text' placeholder='Логин' id='login'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.label} for='fullName'> ФИО </label> 
+                <input className={styles.registerInput} type='text' placeholder='ФИО' id='fullName'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.label} for='email'> Почта </label> 
+                <input className={styles.registerInput} type='email' placeholder='Почта' id='email'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.label} for='password'> Пароль </label> 
+                <input className={styles.registerInput} type='password' placeholder='Пароль' id='password'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+            <label className={styles.label} for='retypePassword'> Повторите пароль </label> 
+                <input className={styles.registerInput} type='password' placeholder='Повторите пароль' id='retypePassword'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.labelBirth} for='dateOfBirth'> Дата рождения </label> 
+                <input className={styles.registerInput} type='date' placeholder='Дата рождения' id='dateOfBirth'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <label className={styles.label} for='contry'> Страна </label> 
+                <input className={styles.registerInput} type='text' placeholder='Страна' id='country'/>
+            </div>
+            <div className={styles.registerInputContainer}>
+                <input className={styles.registerInputCheckbox} type='checkbox' name='showFullName' /> Отображать ФИО другим пользователям
+            </div>
+            <div className={styles.registerInputContainer}>
+                <input className={styles.registerInputCheckbox} type='checkbox' name='showFullName' /> Отображать дату рождения другим пользователям
+            </div>
+        </>
+    )
+}
--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,7 @@
 import styles from './LoginForm.module.css'
 import { useRef, useState } from "react";
+import { LoginFields, RegisterFields  } from "../LoginFields/LoginFields";
+import { FaRegArrowAltCircleLeft, FaRegArrowAltCircleRight } from "react-icons/fa";
 
 const loginToRegister = (elem) => {
     elem.style.transform = 'translateX(0px)';
@@ -16,13 +18,29 @@ export function LoginForm({isLoggedIn}){
         <div className={styles.loginContainerHeight}>
             <div className={styles.loginContainerWidth}>
                 <div className={styles.loginBlock}>
-                    <div className={styles.loginWindow} ref={loginWindow} onClick={
-                        mode == 'login' ? 
-                            () => {loginToRegister(loginWindow.current); setMode('register')} 
-                        : 
-                            () => {registerToLogin(loginWindow.current); setMode('login')}
-                    }>
-                        
+                    <div className={styles.loginWindow} ref={loginWindow}>
+                        {mode == 'login' ? 
+                            <>
+                                <LoginFields />
+                                <div className={styles.loginButton}>
+                                    Войти
+                                </div>
+                                <div className={styles.loginText} onClick={() => {loginToRegister(loginWindow.current);setMode('register')}}>
+                                    <FaRegArrowAltCircleLeft /> Зарегистрироваться
+                                </div>
+                            </>
+                         
+                        :
+                            <> 
+                                <RegisterFields />
+                                <div className={styles.loginButton}>
+                                    Зарегистрироваться
+                                </div>
+                                <div className={styles.loginText} onClick={() => {registerToLogin(loginWindow.current);setMode('login')}}>
+                                     Войти <FaRegArrowAltCircleRight />
+                                </div>
+                            </>
+                        }
                     </div>
                 </div>
             </div>
