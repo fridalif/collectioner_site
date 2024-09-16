d50d6cfee2e663b60aad5dd772eee76a6fa58d53
fridalif <fridalif>
2024-09-16 11:18:03 +0300

    removed getting userId

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -6,11 +6,7 @@ import axios from 'axios';
 export default function Profile(){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const mode = 'Profile';
-    const userId = 0;
-    useEffect(() => {
-        const queryParameters = new URLSearchParams(window.location.search)
-        userId = queryParameters.get("userId")
-    }, [])
+
     axios
     .get('http://127.0.0.1:8000/api/is_logged_in/',{ withCredentials: true })
     .then((response) => {
