8ec6317bf50566eedfdc78b9a74dba87e2b4de8e
fridalif <fridalif>
2024-09-19 14:07:21 +0300

    created get item image url view

--- backend/api/views.py
+++ backend/api/views.py
@@ -202,9 +202,17 @@ def get_items(request:HttpRequest, id=None)->Response:
         print(e)
         return Response({'status':'error','message':'Неизвестная ошибка'})
 
-def get_item_image_urls(request:HttpRequest, item_id:int)->Response:
+
+@api_view(['GET'])
+def get_item_image_urls(request:HttpRequest)->Response:
     try:
-        pass
+        items_ids = request.GET.getlist('items')
+        if len(items_ids) == 0:
+            return Response({'status':'error','message':'Не указаны id предметов'})
+        item_images = ItemImage.objects.filter(item__id__in=items_ids)
+        if request.GET.get('only_main') is not None:
+            item_images = item_images.filter(is_main_image=True)
+        return Response({'status':'ok','data':ItemImageSerializer(item_images,many=True).data})
     except:
         return Response({'status':'error','message':'Неизвестная ошибка'})
 
