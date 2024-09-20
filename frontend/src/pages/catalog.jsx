2dc29e6f392b383e85920a613f8aeb79a3d1075b
fridalif <fridalif>
2024-09-20 12:19:53 +0300

    created catalog page

---
+++ frontend/src/pages/catalog.jsx
@@ -0,0 +1,22 @@
+import {Header} from '../components/Header/Header.jsx';
+import {Body} from '../components/Body/Body.jsx';
+import { useState } from 'react';
+import axios from 'axios';
+
+const serverUrl  = 'http://127.0.0.1:8080';
+export default function Catalog(){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const mode = 'Catalog';
+    axios
+    .get(`${serverUrl}/api/is_logged_in/`,{ withCredentials: true })
+    .then((response) => {
+        setIsLoggedIn(response.data.data['is_logged_in']);
+        console.log(response.data.data['is_logged_in']);
+    })
+    return (
+        <>
+            <Header />
+            <Body isLoggedIn={isLoggedIn} mode={mode}/>
+        </>
+    )
+}
