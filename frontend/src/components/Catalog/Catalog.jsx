c343bc4e9d65d2a0f1e529923d880e8cb94764de
fridalif <fridalif>
2024-09-20 17:02:07 +0300

    created select history moment filter

--- frontend/src/components/Catalog/Catalog.jsx
+++ frontend/src/components/Catalog/Catalog.jsx
@@ -5,11 +5,15 @@ export function Catalog(){
 
     const [worldPart, setWorldPart] = useState(null);
     const [country, setCountry ] = useState(null);
+    const [historyMoment, setHistoryMoment] = useState(null);
+
+
+
     return(
         <div className={styles.catalogContainer}>
             <div className={styles.catalogSideBar}>
                 <div className={styles.catalogSideBarFilterBlock}>
-                    Части света{worldPart}:<br />
+                    Части света:<br />
                     <form>
                         <input type='radio' name='world_part' value='eu' onClick={()=>setWorldPart('eu')}/> Европа <br />
                         <input type='radio' name='world_part' value='as' onClick={()=>setWorldPart('as')}/> Азия <br />
@@ -20,14 +24,14 @@ export function Catalog(){
                 </div>
                 <div className={styles.catalogSideBarFilterBlock}>
                     Страны:<br />
-                    <select>
+                    <select id='selectCountry' onChange={()=>setCountry(document.getElementById('selectCountry').value)}>
                         <option value={null}>{worldPart !== null ? <>Страны</> : <>Выберите часть света</>}</option>
                     </select>
                 </div>
                 <div className={styles.catalogSideBarFilterBlock}>
                     Исторические этапы: <br />
-                    <select>
-                        <option>Страны</option>
+                    <select id='selectHistoryMoment' onChange={()=>setHistoryMoment(document.getElementById('selectHistoryMoment').value)}>
+                        <option value={null}>{country === null ? <>Выберите страну</>:<>Исторические этапы</>}</option>
                     </select>
                 </div>
             </div>
