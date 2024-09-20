6af2abbe5f793c9bb1aec50e43b1c11006aaa8ab
fridalif <fridalif>
2024-09-20 17:57:48 +0300

    created commit filters button

--- frontend/src/components/Catalog/Catalog.jsx
+++ frontend/src/components/Catalog/Catalog.jsx
@@ -17,84 +17,89 @@ export function Catalog(){
     return(
         <div className={styles.catalogContainer}>
             <div className={styles.catalogSideBar}>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Часть света:<br />
-                    <form>
-                        <input type='radio' name='world_part' value='eu' onClick={()=>setWorldPart('eu')}/> Европа <br />
-                        <input type='radio' name='world_part' value='as' onClick={()=>setWorldPart('as')}/> Азия <br />
-                        <input type='radio' name='world_part' value='af' onClick={()=>setWorldPart('af')}/> Африка <br />
-                        <input type='radio' name='world_part' value='am' onClick={()=>setWorldPart('am')}/> Америка <br />
-                        <input type='radio' name='world_part' value='ok' onClick={()=>setWorldPart('ok')}/> Океания <br />
-                    </form>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Страна:<br />
-                    <select id='selectCountry' className={styles.selecter} onChange={()=>setCountry(document.getElementById('selectCountry').value)}>
-                        <option value={null}>{worldPart !== null ? <>Страна</> : <>Выберите часть света</>}</option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Исторический этап: <br />
-                    <select id='selectHistoryMoment' className={styles.selecter} onChange={()=>setHistoryMoment(document.getElementById('selectHistoryMoment').value)}>
-                        <option value={null}>{country === null ? <>Выберите страну</>:<>Исторический этап</>}</option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Категория: <br />
-                    <select id='selectCategory' className={styles.selecter}>
-                        <option value={null}> Все </option>
-                        <option value='mark'> Марка </option>
-                        <option value='philatel'>Филателистический продукт</option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Год: <br />
-                    <div className={styles.inputFromTo}>
-                        <div className={styles.inputFrom}>От <input type="number" className={styles.inputFromToField} id='year_ge'/></div>
-                        <div className={styles.inputFrom}>До <input type="number" className={styles.inputFromToField} id='year_le'/></div>
+                <div className={styles.catalogFilters}>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Часть света:<br />
+                        <form>
+                            <input type='radio' name='world_part' value='eu' onClick={()=>setWorldPart('eu')}/> Европа <br />
+                            <input type='radio' name='world_part' value='as' onClick={()=>setWorldPart('as')}/> Азия <br />
+                            <input type='radio' name='world_part' value='af' onClick={()=>setWorldPart('af')}/> Африка <br />
+                            <input type='radio' name='world_part' value='am' onClick={()=>setWorldPart('am')}/> Америка <br />
+                            <input type='radio' name='world_part' value='ok' onClick={()=>setWorldPart('ok')}/> Океания <br />
+                        </form>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Страна:<br />
+                        <select id='selectCountry' className={styles.selecter} onChange={()=>setCountry(document.getElementById('selectCountry').value)}>
+                            <option value={null}>{worldPart !== null ? <>Страна</> : <>Выберите часть света</>}</option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Исторический этап: <br />
+                        <select id='selectHistoryMoment' className={styles.selecter} onChange={()=>setHistoryMoment(document.getElementById('selectHistoryMoment').value)}>
+                            <option value={null}>{country === null ? <>Выберите страну</>:<>Исторический этап</>}</option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Категория: <br />
+                        <select id='selectCategory' className={styles.selecter}>
+                            <option value={null}> Все </option>
+                            <option value='mark'> Марка </option>
+                            <option value='philatel'>Филателистический продукт</option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Год: <br />
+                        <div className={styles.inputFromTo}>
+                            <div className={styles.inputFrom}>От <input type="number" className={styles.inputFromToField} id='year_ge'/></div>
+                            <div className={styles.inputFrom}>До <input type="number" className={styles.inputFromToField} id='year_le'/></div>
+                        </div>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Эмиссия: <br />
+                        <select id='selectEmission' className={styles.selecter} onChange={()=>setEmission(document.getElementById('selectEmission'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Формат: <br />
+                        <select id='selectFormat' className={styles.selecter} onChange={()=>setFormat(document.getElementById('selectFormat'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Печать: <br />
+                        <select id='selectStamp' className={styles.selecter} onChange={()=>setStamp(document.getElementById('selectStamp'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Цвет: <br />
+                        <select id='selectColor' className={styles.selecter} onChange={()=>setColor(document.getElementById('selectColor'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Клей: <br />
+                        <select id='selectGlue' className={styles.selecter} onChange={()=>setGlue(document.getElementById('selectGlue'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Типография: <br />
+                        <select id='selectPress' className={styles.selecter} onChange={()=>setPress(document.getElementById('selectPress'))}>
+                            <option value={null}> Все </option>
+                        </select>
+                    </div>
+                    <div className={styles.catalogSideBarFilterBlock}>
+                        Водяной знак: <br />
+                        <select id='selectWatermark' className={styles.selecter} onChange={()=>setWatermark(document.getElementById('selectWatermark'))}>
+                            <option value={null}> Все </option>
+                        </select>
                     </div>
                 </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Эмиссия: <br />
-                    <select id='selectEmission' className={styles.selecter} onChange={()=>setEmission(document.getElementById('selectEmission'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Формат: <br />
-                    <select id='selectFormat' className={styles.selecter} onChange={()=>setFormat(document.getElementById('selectFormat'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Печать: <br />
-                    <select id='selectStamp' className={styles.selecter} onChange={()=>setStamp(document.getElementById('selectStamp'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Цвет: <br />
-                    <select id='selectColor' className={styles.selecter} onChange={()=>setColor(document.getElementById('selectColor'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Клей: <br />
-                    <select id='selectGlue' className={styles.selecter} onChange={()=>setGlue(document.getElementById('selectGlue'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Типография: <br />
-                    <select id='selectPress' className={styles.selecter} onChange={()=>setPress(document.getElementById('selectPress'))}>
-                        <option value={null}> Все </option>
-                    </select>
-                </div>
-                <div className={styles.catalogSideBarFilterBlock}>
-                    Водяной знак: <br />
-                    <select id='selectWatermark' className={styles.selecter} onChange={()=>setWatermark(document.getElementById('selectWatermark'))}>
-                        <option value={null}> Все </option>
-                    </select>
+                <div className={styles.catalogCommitFilters}>
+                    Применить фильтры
                 </div>
             </div>
         </div>
