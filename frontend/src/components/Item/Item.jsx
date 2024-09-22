7a5566b2ad3faab3ace90a5878012085efe93db3
fridalif <fridalif>
2024-09-23 00:02:23 +0300

    inserted serverurl

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -1,9 +1,13 @@
 import styles from './Item.module.css';
 import { useState, useEffect } from 'react';
 
-
+const serverUrl = 'http://127.0.0.1';
 export function Item({isLoggedIn}){
     let itemId = 1;
+    const [ imagesList, setImagesList ] = useState(null);
+    const [ currentImage, setCurrentImage ] = useState(0);
+    const [ characteristics, setCharacteristics] = useState(null);
+
     useEffect(()=>{
         const query = new URLSearchParams(window.location.search);
         itemId = query.get('item_id');
@@ -21,7 +25,7 @@ export function Item({isLoggedIn}){
                     </div>
                     <div className={styles.contentMiniImagesColumn}>
                         <div className={styles.contentMiniImage}>
-
+                            <img src='/media/images/avatar.png' style={{width:'45px', height:'45px'}}/>
                         </div>
                         <div className={styles.contentMiniImage}>
                             
